5bd5c827906a70f7c93159b0fd4ab05e13152d4b
Airat <parkourtes@gmail.com>
2022-04-13 15:37:58 +0300

    add poll component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   showForm = false
+
+  polls = [
+    {
+      question: 'Do you like cats or dogs?',
+      image: 'https://images.theconversation.com/files/443350/original/file-20220131-15-1ndq1m6.jpg?ixlib=rb-1.1.0&rect=0%2C0%2C3354%2C2464&q=45&auto=format&w=926&fit=clip',
+      votes: [0, 5, 7],
+      voted: true
+    },
+    {
+      question: 'Best month for summer holiday?',
+      image: 'https://media.istockphoto.com/photos/couple-relax-on-the-beach-enjoy-beautiful-sea-on-the-tropical-island-picture-id1160947136?k=20&m=1160947136&s=612x612&w=0&h=TdExAS2--H3tHQv2tc5woAl7e0zioUVB5dbIz6At0I4=',
+      votes: [1, 6, 4],
+      voted: false
+    },
+  ]
 }
